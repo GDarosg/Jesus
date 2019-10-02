da02b7f0ca2d4cce66c5791fcfe4bcd264ef1143
GustavoDaros <37698225+GDarosg@users.noreply.github.com>
2019-10-02 12:47:39 -0300

    Programa
    Criado a tela de login e implementação sem o banco de dados.
    
    Login admin
    senha admin

---
+++ progama/progama/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin" && textBox2.Text == "admin")
+            {
+                MessageBox.Show("Login efetuado com sucesso");
+                tela2 tela = new tela2();
+                tela.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Login ou senha incorretas");
+            }
+        }
+    }
+}
